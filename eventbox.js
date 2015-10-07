43fc7081ccd2b0b18326071b75651056a29e6713
ydaniv <maggotfish@gmail.com>
2015-10-07 10:39:19 +0300

    Reformat

--- eventbox.js
+++ eventbox.js
@@ -1,156 +1,164 @@
-/**!
+/*!
  * Eventbox Pub/Sub - simple, tiny and robust.
- * Copyright 2011-2013 Yehonatan Daniv under the WTFPL license
- *
- *          DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
- * TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
- *
- * 0. You just DO WHAT THE FUCK YOU WANT TO.
+ * @version 0.2.0
+ * @license BSD Clause 2 License (c) copyright Yehonatan Daniv
+ * https://raw.github.com/ydaniv/eventbox/master/LICENSE
  *
  */
 (function (_window, factory) {
-	if ( typeof define === 'function' && define.amd ) {
-		define(function () {
-			return factory(_window);
-		});
-	} else if ( typeof exports == 'object' ) {
-		module.exports = factory(_window);
-	} else {
-		_window.Eventbox = factory(_window);
-	}
+    if ( typeof define === 'function' && define.amd ) {
+        define(function () {
+            return factory(_window);
+        });
+    }
+    else if ( typeof exports == 'object' ) {
+        module.exports = factory(_window);
+    }
+    else {
+        _window.Eventbox = factory(_window);
+    }
 }(window, function (global) {
 
-	var _self, _scope,
-		Box = {},
-		objToString = global.Object.prototype.toString,
-		_token = 0,
-		emit = global.setImmediate ? emitImmediate : emitTimeout;
+    var _self, _scope,
+        Box = {},
+        objToString = global.Object.prototype.toString,
+        _token = 0,
+        emit = global.setImmediate ? emitImmediate : emitTimeout;
 
 
-	function isObj(obj) {
-		return objToString.call(obj) == '[object Object]';
-	}
+    function isObj (obj) {
+        return objToString.call(obj) == '[object Object]';
+    }
 
 
-	function add(topic, fn, context) {
+    function add (topic, fn, context) {
 
-		var scope = context || _scope;
+        var scope = context || _scope;
 
-		if ( ! (topic in Box) )
-			Box[topic] = {};
+        if ( ! (topic in Box) ) {
+            Box[topic] = {};
+        }
 
-		Box[topic][_token] = scope ? fn.bind(_scope) : fn;
+        Box[topic][_token] = scope ? fn.bind(_scope) : fn;
 
-		return _token += 1;
-	}
-
-
-	function remove(type, token) {
-
-		var t;
-
-		if ( typeof token == 'function' && Box[type] ) {
-			// `token` is a reference to a handler 
-			for ( t in Box[type] )
-				if ( token === Box[type][t] )
-					delete Box[type][t];
-		}
-
-		else if ( Box[type] && Box[type][token] ) {
-			delete Box[type][token];
-		}
-
-		else {
-			delete Box[type];
-		}
-	}
-
-	function emitImmediate(fn, data) {
-		setImmediate(function () {
-			return fn(data);
-		});
-	}
-
-	function emitTimeout(fn, data) {
-		setTimeout(function () {
-			return fn(data);
-		}, 0);
-	}
-
-	return _self = {
-
-		notify	: function (notification, data) {
-
-			var handler, topic;
-
-			if ( isObj(notification) ) {
-
-				for ( topic in notification )
-
-					if ( topic in Box )
-
-						for ( handler in Box[topic] )
-							emit(Box[topic][handler], notification[topic]);
-			}
-
-			else if ( typeof notification == 'string' && notification in Box ) {
-
-				for ( handler in Box[notification] )
-					emit(Box[notification][handler], data);
-
-			}
-
-			return _self;
-		},
+        return _token += 1;
+    }
 
 
-		listen	: function (notification, handler, context) {
+    function remove (type, token) {
 
-			var handlers_map = {}, key;
+        var t;
 
-			if ( isObj(notification) ) {
+        if ( typeof token == 'function' && Box[type] ) {
+            // `token` is a reference to a handler
+            for ( t in Box[type] ) {
+                if ( token === Box[type][t] ) {
+                    delete Box[type][t];
+                }
+            }
+        }
 
-				if ( isObj(handler) ) {
-					context = handler;
-				}
+        else if ( Box[type] && Box[type][token] ) {
+            delete Box[type][token];
+        }
 
-				for ( key in notification )
-					handlers_map[key] = add(key, notification[key], context);
+        else {
+            delete Box[type];
+        }
+    }
 
-			}
+    function emitImmediate (fn, data) {
+        setImmediate(function () {
+            return fn(data);
+        });
+    }
 
-			else if ( typeof notification == 'string' ) {
+    function emitTimeout (fn, data) {
+        setTimeout(function () {
+            return fn(data);
+        }, 0);
+    }
 
-				handlers_map = add(notification, handler, context);
+    return _self = {
 
-			}
+        notify: function (notification, data) {
 
-			if (_scope)
-				_scope = null;
+            var handler, topic;
 
-			return handlers_map;
-		},
+            if ( isObj(notification) ) {
 
+                for ( topic in notification ) {
+                    if ( topic in Box ) {
+                        for ( handler in Box[topic] ) {
+                            emit(Box[topic][handler], notification[topic]);
+                        }
+                    }
+                }
+            }
 
-		unlisten: function (types, idx) {
+            else if ( typeof notification == 'string' && notification in Box ) {
 
-			var topic;
+                for ( handler in Box[notification] ) {
+                    emit(Box[notification][handler], data);
+                }
 
-			if (typeof types == 'string')
-				remove(types, idx);
+            }
 
-			else if ( isObj(types) )
-				for ( topic in types )
-					remove(topic, types[topic]);
-
-			return _self;
-		},
-
-
-		bind	: function (scope) {
-			return _scope = scope, _self;
-		}
+            return _self;
+        },
 
 
-	};
+        listen: function (notification, handler, context) {
+
+            var handlers_map = {}, key;
+
+            if ( isObj(notification) ) {
+
+                if ( isObj(handler) ) {
+                    context = handler;
+                }
+
+                for ( key in notification ) {
+                    handlers_map[key] = add(key, notification[key], context);
+                }
+
+            }
+
+            else if ( typeof notification == 'string' ) {
+
+                handlers_map = add(notification, handler, context);
+
+            }
+
+            if ( _scope ) {
+                _scope = null;
+            }
+
+            return handlers_map;
+        },
+
+
+        unlisten: function (types, idx) {
+
+            var topic;
+
+            if ( typeof types == 'string' ) {
+                remove(types, idx);
+            }
+            else if ( isObj(types) ) {
+                for ( topic in types ) {
+                    remove(topic, types[topic]);
+                }
+            }
+
+            return _self;
+        },
+
+
+        bind: function (scope) {
+            return _scope = scope, _self;
+        }
+
+
+    };
 }));
